bacdfec9c995f15d4f538f86b13177b06a79f556
Benjamin lopez <soybenjadesing@gmail.com>
2025-08-23 13:08:52 -0500

    falta termianr login

--- app/src/main/java/com/geinzz/geinzwork/ui/adapters/ui/pantallas/bottom_sheet_general/ui_bottom_sheet_login.kt
+++ app/src/main/java/com/geinzz/geinzwork/ui/adapters/ui/pantallas/bottom_sheet_general/ui_bottom_sheet_login.kt
@@ -1,19 +1,30 @@
 package com.geinzz.geinzwork.ui.adapters.ui.pantallas.bottom_sheet_general
 
+import android.icu.util.Calendar
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.TextButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDefaults
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
@@ -21,6 +32,8 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -29,13 +42,20 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.PopupProperties
 import com.geinzz.geinzwork.ui.adapters.ui.COMP_principal_filtrado.retornar_pleaceholder_label
 import com.geinzz.geinzwork.ui.adapters.ui.COMP_principal_filtrado.texto_generico_one_line
 import com.joelkanyi.jcomposecountrycodepicker.component.KomposeCountryCodePicker
 import com.joelkanyi.jcomposecountrycodepicker.component.rememberKomposeCountryCodePickerState
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
+val options_genero = listOf("Masculino", "Femenino", "Otro")
+val opciones_localida = listOf("Barranca", "Supe", "Puerto supe", "Paramonga", "Pativilca")
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -72,9 +92,11 @@ fun componentes_crear_cuenta() {
     var errorLocalidad by remember { mutableStateOf(false) }
     var errorFechaNacimiento by remember { mutableStateOf(false) }
     var errorTelefono by remember { mutableStateOf(false) }
+
+    var show_dialog by remember { mutableStateOf(false) }
     LazyColumn(
         modifier = Modifier
-            .padding(10.dp)
+            .padding(10.dp).fillMaxHeight(0.90f)
     ) {
         // nombre
         item {
@@ -107,7 +129,7 @@ fun componentes_crear_cuenta() {
                 placeholderText = "Escribe tu nombre de usuario",
                 texto_error = "El campo es obligatorio",
                 isError = errorUsername
-                )
+            )
         }
         // Correo electrónico
         item {
@@ -127,31 +149,22 @@ fun componentes_crear_cuenta() {
         }
         // Género
         item {
-            MyOutlinedTextField(
-                value = genero,
-                onValueChange = { genero = it },
-                labelText = "Selecciona tu género",
-                placeholderText = "Selecciona tu género",
-              
-            )
+            ExpandDropDown(options_genero, "Seleciona tu genero")
         }
         // Localidad
         item {
-            MyOutlinedTextField(
-                value = localidad,
-                onValueChange = { localidad = it },
-                labelText = "Selecciona tu localidad",
-                placeholderText = "Selecciona tu localidad",
-            )
+            ExpandDropDown(opciones_localida, "Seleciona tu localidad")
         }
         // Fecha de nacimiento
         item {
-            MyOutlinedTextField(
-                value = fechaNacimiento,
-                onValueChange = { fechaNacimiento = it },
-                labelText = "Ingresa tu fecha de nacimiento",
-                placeholderText = "Ingresa tu fecha de nacimiento",
-            )
+//            MyOutlinedTextField(
+//                value = fechaNacimiento,
+//                onValueChange = { fechaNacimiento = it },
+//                labelText = "Ingresa tu fecha de nacimiento",
+//                placeholderText = "Ingresa tu fecha de nacimiento",
+//            )
+            DateButton()
+
         }
         //crear cuenta
         item {
@@ -272,6 +285,131 @@ fun verificarCampo(valor: String): Boolean {
 }
 
 @Composable
-fun iniciar_seccion() {
+fun dop_down_menu_genero() {
+    var expanded by remember { mutableStateOf(false) }
+    Box(modifier = Modifier) {
+        Button(onClick = { expanded = true }) {
+            texto_generico_one_line("ver opciones")
+        }
 
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            properties = PopupProperties(dismissOnClickOutside = true, dismissOnBackPress = true)
+        ) {
+            DropdownMenuItem(
+                text = { texto_generico_one_line("masculino") },
+                onClick = { expanded = false })
+            DropdownMenuItem(
+                text = { texto_generico_one_line("femenino") },
+                onClick = { expanded = false })
+            DropdownMenuItem(
+                text = { texto_generico_one_line("otro") },
+                onClick = { expanded = false })
+        }
+    }
+
+}
+
+@Composable
+fun DateButton() {
+    var showDialog by remember { mutableStateOf(false) }
+    var selectedDate by remember { mutableStateOf("") }
+
+    Button(onClick = { showDialog = true }) {
+        Text(if (selectedDate.isEmpty()) "Seleccionar fecha" else selectedDate)
+    }
+
+    DatePickerExample(
+        showDialog = showDialog,
+        onDismiss = { showDialog = false },
+        onDateSelected = { millis ->
+            millis?.let {
+                val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+                selectedDate = sdf.format(Date(it))
+            }
+        }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ExpandDropDown(lista: List<String>, lable: String) {
+    var expanded by remember { mutableStateOf(false) }
+    var selected by remember { mutableStateOf("") }
+
+
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = { expanded = !expanded },
+        modifier = Modifier
+            .padding(10.dp)
+            .clip(RoundedCornerShape(30)) // alterna entre abrir/cerrar
+    ) {
+        TextField(
+            value = selected,
+            onValueChange = {},
+            readOnly = true,
+            label = { Text(lable) },
+            modifier = Modifier
+                .menuAnchor()
+                .fillMaxWidth(),
+            trailingIcon = {
+                ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+            },
+            colors = ExposedDropdownMenuDefaults.textFieldColors()
+        )
+
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            lista.forEach { option ->
+                DropdownMenuItem(
+                    text = { Text(option) },
+                    onClick = {
+                        selected = option
+                        expanded = false
+                    }
+                )
+            }
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DatePickerExample(
+    showDialog: Boolean,
+    onDismiss: () -> Unit,
+    onDateSelected: (Long?) -> Unit
+) {
+    val calendar = Calendar.getInstance().apply {
+        add(Calendar.DAY_OF_YEAR, +1)
+        set(Calendar.MONTH, Calendar.JANUARY)
+    }
+
+    val datePickerState = rememberDatePickerState(
+        initialSelectedDateMillis = calendar.timeInMillis,
+        initialDisplayedMonthMillis = calendar.timeInMillis,
+        yearRange = 2024..2025
+    )
+
+    if (showDialog) {
+        DatePickerDialog(
+            onDismissRequest = onDismiss,
+            confirmButton = {
+                TextButton(onClick = {
+                    onDateSelected(datePickerState.selectedDateMillis)
+                    onDismiss()
+                }) {
+                    texto_generico_one_line("Confirmar")
+                }
+            },
+            colors = DatePickerDefaults.colors()
+        ) {
+            DatePicker(state = datePickerState)
+        }
+    }
 }
